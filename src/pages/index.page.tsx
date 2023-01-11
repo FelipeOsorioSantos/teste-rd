0baf473b9040e220110226f5937743cafe36a5c7
Ariel Christmann <ariel.christmann@stage.consulting>
2023-01-11 15:53:13 -0300

    feat: added page title

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,6 @@
 import { Flex, Image } from '@chakra-ui/react';
 import { NextPage } from 'next';
+import Head from 'next/head';
 import { AppGlobalContainer } from '../components/_app/AppGlobalContainer';
 import { FormAnaliseCredito } from '../components/analise-credito/FormAnaliseCredito';
 import { useWindowSize } from '../hooks/useWindowSize';
@@ -8,38 +9,49 @@ const Home: NextPage = () => {
   const isLargerThan768 = useWindowSize();
 
   return (
-    <Flex bg="#CA004F" h="100vh" w="100vw" position="relative">
-      <Flex display="block" position="absolute" zIndex="0" h="100vh" w="100vw">
-        <Image
-          display={isLargerThan768 ? 'flex' : 'none'}
-          h="100%"
-          src="./analise-credito/hero1.webp"
-          alt="Análise de Crédito"
-          objectFit="cover"
+    <>
+      <Head>
+        <title>Shopbanx - Solicitação de crédito</title>
+      </Head>
+      <Flex bg="#CA004F" h="100vh" w="100vw" position="relative">
+        <Flex
+          display="block"
           position="absolute"
-          pl="5%"
-          pt="30px"
-        />
-        <Image
-          w="100%"
-          objectFit="cover"
-          h="100%"
-          src="./analise-credito/bg.webp"
-          alt="Análise Background"
-        />
-      </Flex>
-
-      <AppGlobalContainer
-        zIndex="1"
-        justifyContent={isLargerThan768 ? 'center' : 'flex-start'}
-        overflowY={'scroll'}
-        mb={isLargerThan768 ? '0px' : '32px'}
-      >
-        <Flex justifyContent={isLargerThan768 ? 'flex-end' : 'center'}>
-          <FormAnaliseCredito />
+          zIndex="0"
+          h="100vh"
+          w="100vw"
+        >
+          <Image
+            display={isLargerThan768 ? 'flex' : 'none'}
+            h="100%"
+            src="./analise-credito/hero1.webp"
+            alt="Análise de Crédito"
+            objectFit="cover"
+            position="absolute"
+            pl="5%"
+            pt="30px"
+          />
+          <Image
+            w="100%"
+            objectFit="cover"
+            h="100%"
+            src="./analise-credito/bg.webp"
+            alt="Análise Background"
+          />
         </Flex>
-      </AppGlobalContainer>
-    </Flex>
+
+        <AppGlobalContainer
+          zIndex="1"
+          justifyContent={isLargerThan768 ? 'center' : 'flex-start'}
+          overflowY={'scroll'}
+          mb={isLargerThan768 ? '0px' : '32px'}
+        >
+          <Flex justifyContent={isLargerThan768 ? 'flex-end' : 'center'}>
+            <FormAnaliseCredito />
+          </Flex>
+        </AppGlobalContainer>
+      </Flex>
+    </>
   );
 };
 
