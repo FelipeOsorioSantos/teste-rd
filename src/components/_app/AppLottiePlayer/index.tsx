c9a476001b91b814c7f3d21497b18a0f8f4a933e
FelipeOsorioSantos <felipe.osorio@stage.consulting>
2023-10-11 09:47:00 -0300

    teste1

--- src/components/_app/AppLottiePlayer/index.tsx
+++ src/components/_app/AppLottiePlayer/index.tsx
@@ -11,7 +11,7 @@ export const AppLottiePlayer = ({
 }: AppLottiePlayerProps) => {
   return (
     <Flex>
-      <Player autoplay loop src={`./lottie/${src}`} {...otherProps} />
+      <Player autoplay loop src={`./lottie/${src}`} />
     </Flex>
   );
 };
